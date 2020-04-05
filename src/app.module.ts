12618aa3aeb4292cc65eff07b20b9333966ce56b
Alejandro <alejandrofpo@gmail.com>
2020-04-06 00:31:39 +0200

    Remove unused db modules and stay with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'users/users.module';
 
-import { MongoModule } from 'database/mongo-db.module';
-import { MySQLModule } from 'database/my-sql.module';
 import { PostgreSQLModule } from 'database/postgre-sql.module';
 
 @Module({
   imports: [
-    MongoModule,
-    // MySQLModule,
-    // PostgreSQLModule,
+    PostgreSQLModule,
     UsersModule,
   ],
   controllers: [AppController],
